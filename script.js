fa60da457bdb6de38ff5ccc67b71ad2faa8a6c50
Ghidon <61509663+Ghidon@users.noreply.github.com>
2020-03-04 18:06:48 +0200

    Milestone5 Completed
    Removed old function
    added fetching from url modified with x number coming from input
    transformed data into Json
    added a function to assign to variable y the result of data.
    passing y into html

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function fib(x) {
-    if (x == 0) return 0;
-    if (x <= 2) return 1;
+//function fib(x) {
+//  if (x == 0) return 0;
+//if (x <= 2) return 1;
+//
+//  return fib(x - 1) + fib(x - 2);
+//}
 
-    return fib(x - 1) + fib(x - 2);
-}
+
+//pass x to a request to server
+//get an answer from the server and assign it to:
+// document.getElementById("ynumber").innerText
 
 
 function getX() {
     let x = document.getElementById("xnumber").value;
-    document.getElementById("ynumber").innerText = fib(x);
+    let url = "http://localhost:5050/fibonacci/" + x
+    console.log("hello")
+    fetch(url)
+        .then(res => res.json())
+        .then(function (data) {
+            let y = data.result;
+            document.getElementById("ynumber").innerText = y
 
+        })
 }
+
+
