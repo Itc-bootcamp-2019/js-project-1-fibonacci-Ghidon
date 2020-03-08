0cae40671c3d678743462c39792a4edfcf84c19d
Ghidon <61509663+Ghidon@users.noreply.github.com>
2020-03-08 13:04:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
-//function fib(x) {
-//  if (x == 0) return 0;
-//if (x <= 2) return 1;
-//
-//  return fib(x - 1) + fib(x - 2);
-//}
+function fib(x) {
+    if (x == 0) return 0;
+    if (x <= 2) return 1;
+
+    return fib(x - 1) + fib(x - 2);
+}
 
 
 //pass x to a request to server
 //get an answer from the server and assign it to:
 // document.getElementById("ynumber").innerText
-
+function stopLoader() {
+    document.getElementById("Loader").classList.add('loaded');
+}
 
 function getX() {
     let x = document.getElementById("xnumber").value;
-    let url = "http://localhost:5050/fibonacci/" + x
-    console.log("hello")
-    fetch(url)
-        .then(res => res.json())
-        .then(function (data) {
-            let y = data.result;
-            document.getElementById("ynumber").innerText = y
+    document.getElementById("ynumber").innerText = ""
+    if (x > 50) {
+        document.getElementById("50Error").classList.remove('loaded');
+        document.getElementById("xnumber").classList.add("inputError");
+    } else {
+        document.getElementById("50Error").classList.add('loaded');
+        document.getElementById("xnumber").classList.remove("inputError");
+        let url = "http://localhost:5050/fibonacci/" + x;
+        document.getElementById("Loader").classList.remove('loaded');
+        fetch(url)
 
-        })
+            .then(
+                function (response) {
+                    if (response.status !== 200) {
+                        document.getElementById("errText").classList.remove('loaded');
+                        throw response
+
+                    } else {
+                        document.getElementById("errText").classList.add('loaded');
+                        return response.json();
+                    }
+                }
+            )
+            .then(function (data) {
+                let y = data.result;
+                stopLoader();
+                document.getElementById("ynumber").innerText = y;
+
+            })
+            .catch(error => error.text())
+            .then(meaning => {
+                console.log(meaning);
+                stopLoader();
+                let error42 = "Server Error: " + meaning
+                document.getElementById("errText").innerText = error42;
+
+            })
+    }
+
 }
-
-
